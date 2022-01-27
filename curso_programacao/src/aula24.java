7261b00f78b497896254a7be5f93e4fdb873a264
kellyortiz_ <keortizf@gmail.com>
2022-01-27 17:10:52 -0300

    Atividades propostas durante as aulas do curso de java

---
+++ curso_programacao/src/aula24.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+
+public class aula24 {
+
+	public static void main(String[] args) {
+
+		int y = 32;
+		int idade = 31;
+		double renda = 4000.0;
+		double x = 10.35784;
+		String nome = "Maria";
+		
+
+		System.out.println("Numero inteiro: " + x);
+		
+		// Antes do locale fica com virgula o double
+		System.out.printf("Formatação de duas casas decimais: " + "%.2f%n", x);
+		System.out.printf("Formatação de quatro casas decimais: " + "%.4f%n", x);
+		
+		// Depois do locale fica com ponto o double
+		Locale.setDefault(Locale.US);
+		System.out.printf("Formatação de duas casas decimais com locale: " + "%.2f%n", x);
+		System.out.printf("Formatação de quatro casas decimais com locale: " + "%.4f%n", x);
+		
+		// Concatenar elementos
+		
+		System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+
+	}
+
+}
---
+++ curso_programacao/src/aula25.java
@@ -0,0 +1,16 @@
+
+public class aula25 {
+
+	public static void main(String[] args) {
+		
+		double b, B, h, area;
+		b = 6f;
+		B = 8f;
+		h = 5f;
+		
+		area = (b + B) / 2.0 * h;
+		System.out.println("Valor da area é: " + area);
+
+	}
+
+}
