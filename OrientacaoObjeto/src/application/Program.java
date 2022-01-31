52ca18c6f264216daf974dd0c2571f94079b8cb1
kellyortiz_ <keortizf@gmail.com>
2022-01-31 14:56:08 -0300

    Criação da função área

--- OrientacaoObjeto/src/application/Program.java
+++ OrientacaoObjeto/src/application/Program.java
@@ -25,12 +25,9 @@ public class Program {
 		y.a = sc.nextDouble();
 		y.b = sc.nextDouble();
 		y.c = sc.nextDouble();
-		
-		double p = (x.a + x.b + x.c) / 2.0;
-		double areaX = Math.sqrt(p * (p - x.a) * (p - x.a) * (p - x.c));
-		
-		p = (y.a + y.b + y.c) / 2.0;
-		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+				
+		double areaX = x.area();
+		double areaY = y.area();
 		
 		System.out.printf("Area do triangulo x é: %.4f%n", areaX);
 		System.out.printf("Area do triangulo y é: %.4f%n", areaY);
